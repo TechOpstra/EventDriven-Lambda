a8241bf30295b370dc14bc660327864c8291645b
Harish <107391575+8555936327@users.noreply.github.com>
2025-05-14 16:04:17 +0530

    Update backend.tf

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     bucket       = "techopsbucket123"
-    key          = "vpcec2/terraform.tfstateeventlambd1"
+    key          = "vpcec2/terraform.tfstateeventlambd2"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
