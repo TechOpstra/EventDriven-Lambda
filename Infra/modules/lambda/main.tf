40023405df184472498f3c1d71b9db462dc175b6
Harish <107391575+8555936327@users.noreply.github.com>
2025-05-14 15:33:37 +0530

    Update main.tf

--- Infra/modules/lambda/main.tf
+++ Infra/modules/lambda/main.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "this" {
   role          = aws_iam_role.lambda_exec.arn
 
   source_code_hash = filebase64sha256("lambda_function_payload.zip")
-  filename         = "lambda_function_payload.zip"
+  filename         = "lambda_payload.zip"
 }
 
 resource "aws_iam_role" "lambda_exec" {
